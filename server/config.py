46432880ddd36b321db72a0e5d92cc6b411d5449
Szymon Pawłowski <112553685+Mouson12@users.noreply.github.com>
2025-01-08 10:53:04 +0100

    add secret key

--- server/config.py
+++ server/config.py
@@ -8,3 +8,4 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
+    SECRET_KEY = 'Arius2024'
