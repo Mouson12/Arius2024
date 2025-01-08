f950661c46674f3226aeb68d827e942d6fbcac6b
Szymon <pawlowski.szymek@gmail.com>
2025-01-08 12:22:17 +0000

    fix email function

--- server/config.py
+++ server/config.py
@@ -2,10 +2,11 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@garage_db:5432/garage_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = 'smtp.sendgrid.net'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = 'apikey'
+    MAIL_PASSWORD = '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'
+    MAIL_DEFAULT_SENDER = 'warsztat.arius@gmail.com'
     SECRET_KEY = 'Arius2024'
